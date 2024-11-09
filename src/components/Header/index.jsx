35d8487f69267f68bcbe8fc3fecb73838821e7c2
piirk <baptiste.boucharechas@gmail.com>
2024-11-09 18:49:02 +0100

    body height and header design

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom'
+import './Header.scss'
 
 const Header = () => {
   return (
-    <header>
-      <img src="/logo.svg" alt="Logo" />
-      <nav>
+    <header className="kasa-header">
+      <img src="/logo.svg" alt="Logo" className="kasa-header__logo" />
+      <nav className="kasa-header__nav">
         <Link to="/">Accueil</Link>
         <Link to="/about">A propos</Link>
       </nav>
