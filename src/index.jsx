8a7d8a33e55861dd62729bbd73226dc774c292be
piirk <baptiste.boucharechas@gmail.com>
2024-11-09 15:41:14 +0100

    add About page and routing

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/'
+import About from './pages/About/'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -9,6 +10,7 @@ root.render(
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   </React.StrictMode>,
