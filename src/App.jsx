f542241e67c4fb6bb9d7fee4759290b14668fca1
piirk <baptiste.boucharechas@gmail.com>
2024-11-09 15:18:54 +0100

    move App

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/App.css';
+import Home from './pages/Home/index';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Home />
+      </header>
+    </div>
+  );
+}
+
+export default App;
