291a62c7e3a628863ef7f057454fe920cccc5f10
piirk <baptiste.boucharechas@gmail.com>
2024-11-09 16:05:11 +0100

    basic Error for 404

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home/'
 import About from './pages/About/'
 import Header from './components/Header/'
 import Footer from './components/Footer/'
+import Error from './components/Error/'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -19,6 +20,7 @@ root.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
     </Router>
