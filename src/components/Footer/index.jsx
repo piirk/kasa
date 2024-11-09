c40b2038b21a18fc761184225b502960a7c1102d
piirk <baptiste.boucharechas@gmail.com>
2024-11-09 16:31:53 +0100

    reset css, font and footer design

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,8 +3,8 @@ import './Footer.scss'
 const Footer = () => {
   return (
     <footer className="kasa-footer">
-      <img src="/logo-footer.svg" alt="Logo" />
-      <p>© 2020 Kasa. All rights reserved</p>
+      <img src="/logo-footer.svg" alt="Logo" className="kasa-footer__logo" />
+      <p className="kasa-footer__copy">© 2020 Kasa. All rights reserved</p>
     </footer>
   )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './index.scss'
 import Home from './pages/Home/'
 import About from './pages/About/'
 import Header from './components/Header/'
