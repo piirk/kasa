9d6a6501c4931df762697a0e464634b86c126c6c
piirk <baptiste.boucharechas@gmail.com>
2024-11-09 15:43:53 +0100

    basic Header with nav

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/'
 import About from './pages/About/'
+import Header from './components/Header/'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
