2848f0164ba1cd8afea074ae8ea9482cb2f95b47
piirk <baptiste.boucharechas@gmail.com>
2024-11-09 16:00:28 +0100

    update for yarn only and futur react-router v7

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer>
+      <img src="/logo.svg" alt="Logo" />
+      <p>© 2020 Kasa. All rights reserved</p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/index.jsx
+++ src/index.jsx
@@ -4,16 +4,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/'
 import About from './pages/About/'
 import Header from './components/Header/'
+import Footer from './components/Footer/'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
+      <Footer />
     </Router>
   </React.StrictMode>,
 )
