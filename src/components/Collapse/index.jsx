ae9383ae9094f6999c8e5aa826ad90d3183e3119
piirk <baptiste.boucharechas@gmail.com>
2024-11-10 10:42:14 +0100

    Collapse design wip

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,11 +1,12 @@
 import './Collapse.scss'
+import arrow from './assets/arrow.svg'
 
 const Collapse = ({ title, content }) => {
   return (
     <details className="kasa-collapse">
       <summary>
         {title}
-        <span className="marker"></span>
+        <img src={arrow} alt="arrow" className="arrow"></img>
       </summary>
       <p>{content}</p>
     </details>
