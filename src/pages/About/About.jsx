91ed9c41074fabbbae0a0a68b07dfdc02ecaf78a
piirk <baptiste.boucharechas@gmail.com>
2024-11-13 14:08:37 +0100

    faking datas for Collapse components in About page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,27 +2,37 @@ import Banner from '../../components/Banner/Banner'
 import Collapse from '../../components/Collapse/Collapse'
 import './About.scss'
 
+const mockData = [
+  {
+    title: 'Fiabilité',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget felis nec dolor vehicula tincidunt. Nullam euismod, purus nec ultricies ultricies, mi tortor ultricies metus, at lacinia magna odio nec nunc. Donec et velit sit amet ligula eleifend rutrum. Nulla facilisi. Curabitur sed nisl sit amet lacus luctus tincidunt. Integer nec lacus in augue lacinia luctus. Sed nec risus ac justo ultrices lacinia. Nam auctor, sapien quis fermentum aliquet, mi nisi fermentum metus, eget ultricies nunc nunc nec nunc. Nullam nec nunc nec nunc.',
+  },
+  {
+    title: 'Respect',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget felis nec dolor vehicula tincidunt. Nullam euismod, purus nec ultricies ultricies, mi tortor ultricies metus, at lacinia magna odio nec nunc. Donec et velit sit amet ligula eleifend rutrum. Nulla facilisi. Curabitur sed nisl sit amet lacus luctus tincidunt. Integer nec lacus in augue lacinia luctus. Sed nec risus ac justo ultrices lacinia. Nam auctor, sapien quis fermentum aliquet, mi nisi fermentum metus, eget ultricies nunc nunc nec nunc. Nullam nec nunc nec nunc.',
+  },
+  {
+    title: 'Service',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget felis nec dolor vehicula tincidunt. Nullam euismod, purus nec ultricies ultricies, mi tortor ultricies metus, at lacinia magna odio nec nunc. Donec et velit sit amet ligula eleifend rutrum. Nulla facilisi. Curabitur sed nisl sit amet lacus luctus tincidunt. Integer nec lacus in augue lacinia luctus. Sed nec risus ac justo ultrices lacinia. Nam auctor, sapien quis fermentum aliquet, mi nisi fermentum metus, eget ultricies nunc nunc nec nunc. Nullam nec nunc nec nunc.',
+  },
+  {
+    title: 'Sécurité',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget felis nec dolor vehicula tincidunt. Nullam euismod, purus nec ultricies ultricies, mi tortor ultricies metus, at lacinia magna odio nec nunc. Donec et velit sit amet ligula eleifend rutrum. Nulla facilisi. Curabitur sed nisl sit amet lacus luctus tincidunt. Integer nec lacus in augue lacinia luctus. Sed nec risus ac justo ultrices lacinia. Nam auctor, sapien quis fermentum aliquet, mi nisi fermentum metus, eget ultricies nunc nunc nec nunc. Nullam nec nunc nec nunc.',
+  },
+]
+
 const About = () => {
   return (
     <div className="kasa-container">
       <Banner img="about" />
       <div className="kasa-collapses">
-        <Collapse
-          title="Fiabilité"
-          content="Lorem ipsum odor amet, consectetuer adipiscing elit. Libero fermentum nisl dui ipsum facilisis platea. Nisl natoque aenean viverra sit himenaeos parturient per bibendum? Nisi ridiculus in sed; interdum sagittis enim adipiscing tempus. Velit erat platea dapibus feugiat senectus vehicula. Neque potenti mus augue auctor ad. Dapibus semper at posuere sodales justo semper ipsum sapien. Donec vehicula quisque cras justo non litora elementum. Diam faucibus sodales curae rutrum dui."
-        />
-        <Collapse
-          title="Respect"
-          content="Lorem ipsum odor amet, consectetuer adipiscing elit. Libero fermentum nisl dui ipsum facilisis platea. Nisl natoque aenean viverra sit himenaeos parturient per bibendum? Nisi ridiculus in sed; interdum sagittis enim adipiscing tempus. Velit erat platea dapibus feugiat senectus vehicula. Neque potenti mus augue auctor ad. Dapibus semper at posuere sodales justo semper ipsum sapien. Donec vehicula quisque cras justo non litora elementum. Diam faucibus sodales curae rutrum dui."
-        />
-        <Collapse
-          title="Service"
-          content="Lorem ipsum odor amet, consectetuer adipiscing elit. Libero fermentum nisl dui ipsum facilisis platea. Nisl natoque aenean viverra sit himenaeos parturient per bibendum? Nisi ridiculus in sed; interdum sagittis enim adipiscing tempus. Velit erat platea dapibus feugiat senectus vehicula. Neque potenti mus augue auctor ad. Dapibus semper at posuere sodales justo semper ipsum sapien. Donec vehicula quisque cras justo non litora elementum. Diam faucibus sodales curae rutrum dui."
-        />
-        <Collapse
-          title="Sécurité"
-          content="Lorem ipsum odor amet, consectetuer adipiscing elit. Libero fermentum nisl dui ipsum facilisis platea. Nisl natoque aenean viverra sit himenaeos parturient per bibendum? Nisi ridiculus in sed; interdum sagittis enim adipiscing tempus. Velit erat platea dapibus feugiat senectus vehicula. Neque potenti mus augue auctor ad. Dapibus semper at posuere sodales justo semper ipsum sapien. Donec vehicula quisque cras justo non litora elementum. Diam faucibus sodales curae rutrum dui."
-        />
+        {mockData.map((data, index) => (
+          <Collapse key={index} title={data.title} content={data.content} />
+        ))}
       </div>
     </div>
   )
